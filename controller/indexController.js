1ad6290bfd79dc4180af8f27c6ef6ebacee5e57e
pallavi kamboj <pallavikamboj267@gmail.com>
2020-02-02 23:49:14 +0530

    controller

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+/*const User = require('../models/user');
 const Message = require('../models/message');
 const validator = require('express-validator');
 var passport = require('passport');
@@ -79,6 +79,7 @@ exports.signUp_post =
             })
             return
           }*/
+          /*
         else
         {
             User.findOne({username: username})
@@ -145,6 +146,208 @@ exports.login_post = (req,res,next)=>
     passport.authenticate('local',{
         successRedirect:'/messages',
         failureRedirect:'/sign-up',
+        failureFlash: true
+    })(req,res,next);
+}
+//create message get request
+exports.create_message_get = (req,res)=>
+{
+    console.log(req.user);
+   /* if(!req.user)
+    {
+        res.redirect('login',{message: "you need to login to view all the messages"});
+    }*/
+   /* res.render('create-message');
+}
+
+//create message post request
+exports.create_message_post = (req,res,next)=>
+{
+    check('msg_title').isLength({min:1}).trim().withMessage('title cannot be empty'),
+    check('msg').isLength({min:1}).trim().withMessage('message cannot be empty'),
+    sanitizeBody('msg_title').escape(),
+    sanitizeBody('msg').escape()
+
+        console.log(req.user);
+    const message = new Message(
+        {
+            msg_title: req.body.title,
+            msg: req.body.message,
+            //user: req.user.id
+        }
+    ).save(err=>
+        {
+            if(err)
+            {
+                return next(err);
+            }
+            res.redirect('/messages');
+        });
+        
+
+};
+//display all messages
+exports.get_messages = (req,res,next)=>
+{
+    Message.find({})
+    .exec((err,messages)=>
+    {
+        if(err)
+        {
+            return next(err);
+        }
+        res.render('all_messages',{messages:messages});
+    });
+};
+*/
+const User = require('../models/user');
+const Message = require('../models/message');
+const validator = require('express-validator');
+var passport = require('passport');
+var bcrypt = require('bcrypt');
+const { check, validationResult } = require('express-validator');
+const { forwardAuthenticated } = require('../config/auth');
+
+
+//validate fields
+const validateAndSanitize = [
+    validator.body('firstname','firstname cannot be empty and cannot have special characters')
+    .trim()
+    .isLength({min:1})
+    .isAlphanumeric(),
+    validator.body('lastname','lastname cannot be empty and cannot have special characters')
+    .trim()
+    .isLength({min:1})
+    .isAlphanumeric(),
+    validator.body('username','username cannot be empty and cannot have special characters')
+    .trim()
+    .isLength({min:1})
+    .isAlphanumeric(),
+    validator.body('password','firstname cannot be empty and cannot have special characters')
+    .trim()
+    .isLength({min:1})
+    .isAlphanumeric(),
+    validator.body('confirm_password', 'Passwords do not match, try again')
+		.not()
+		.isEmpty()
+		.exists()
+		.custom((value, { req }) => value === req.body.password),
+    
+    
+   
+    //sanitize body
+    validator.sanitizeBody(
+		['userName', 'firstName', 'lastName', 'password', 'confirm_password']
+	).escape()
+]
+//home page
+exports.index = function(req,res,next)
+{
+    res.render('index');
+};
+//sign up get
+exports.signUp_get = (req,res)=>
+{
+    
+    res.render('signUp');
+};
+
+
+
+//sign up post
+exports.signUp_post =
+[
+    validateAndSanitize,
+    
+   
+    (req,res,next)=>
+    {
+        const {firstname, lastname, username, password} = req.body;
+        console.log(req.body.password);
+        console.log(req.body.confirm_password);
+        const formErrors = [];
+        const errors = validator.validationResult(req);
+        /*if(!errors.isEmpty())
+        {
+            console.log(`errors: ${JSON.stringify(errors)}`);
+            res.render('signUp',{errors: errors});
+        }*/
+        if (!errors.isEmpty()) 
+        {
+            res.render('signup', {
+              title: 'Sign Up',
+              
+              errors: errors.array()
+            })
+            return
+          }
+        else
+        {
+            User.findOne({username: username})
+        .then(user =>
+            {
+                if(user)
+                {
+                    console.log('username already exsits');
+                    res.render('signUp',{user:user});
+                }
+                else{
+                    bcrypt.genSalt(10, (err,salt)=>
+                    {
+                        if(err)
+                        {
+                            console.log(err);
+                            throw err;
+                        }
+                        bcrypt.hash(password,salt,(err,hashedPassword)=>
+                        {
+                            if(err)
+                            {
+                                console.log(err);
+                                throw err;
+                            }
+                            const newUser = new User(
+                                {
+                                    firstname: firstname,
+                                    lastname: lastname,
+                                    username: username,
+                                    password: hashedPassword
+                                }
+                            )
+                            newUser.save(err=>
+                                {
+                                    if(err)
+                                    {
+                                        console.log(err);
+                                        throw err;
+                                    }
+                                    console.log("sign up successfully!! login to view all messages");
+                                    res.redirect('/login');
+                                });
+
+                        })
+
+                    })
+                }
+            })
+    }
+}
+]
+
+//login get
+exports.login_get = (req,res)=>
+{
+    res.render('login');
+}
+//login post 
+
+
+exports.login_post = (req,res,next)=>
+{
+    console.log("inside login post");
+    passport.authenticate('local',{
+        successRedirect:'/messages',
+        failureRedirect:'/login',
         failureFlash: true
     })(req,res,next);
 }
