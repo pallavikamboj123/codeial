d418d00dcc6a356b70ab327f7affe0dae7275c6e
pallavi kamboj <pallavikamboj267@gmail.com>
2020-02-06 07:33:33 +0530

    models

--- models/message.js
+++ models/message.js
@@ -6,7 +6,8 @@ var msgSchema = new schema({
     msg_title:{type:String,required:true,min:1},
     msg: {type:String,required:true,min:1},
     timestamp: {type: Date,required:true,default:Date.now},
-    user:{type:schema.Types.ObjectId,ref:'users'}
+    //user:{type:schema.Types.ObjectId,ref:'users'}
+    user:{type:schema.Types.ObjectId, ref:'user'},
 });
 msgSchema.virtual('dateFormatted').get(function(){
     return moment(this.timestamp).format('YYYY-MM-DD');
