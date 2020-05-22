aad8baa34cd1c7c29d197712245d4ca692f987c4
amrik malhans <amrikdeveloper@gmail.com>
2020-05-22 11:18:37 +0530

    db code change

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,14 +1,20 @@
 const Post = require('../models/post');
 
+
 module.exports.create = (req,res)=>{
-    Post.create({
+    // creating a new post and saving to database according to defined schema
+    const newPost = new Post({
         content: req.body.content,
         user: req.user._id
-    },(err,post)=>{
-        if(err){
-            console.log("error in creating a post");
-            return;
-        }
-        return res.redirect('back');
-    });
+    })
+    //finally saving to db
+   newPost.save()
+   //after saving logging the data 
+   .then(data => {
+       console.log(data);
+   })
+   //error handling as usual
+   .catch(err => {
+       console.error(err)
+   })
 }
